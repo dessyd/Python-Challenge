e29f4308830e41b1f49c62751f3bb11e2157b930
dessyd <reprendre.mezzo_0j@icloud.com>
2023-01-21 17:58:15 +0100

    First Challenge

---
+++ 1.py
@@ -0,0 +1,11 @@
+riddle = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
+answer = ""
+
+for c in riddle:
+  pos = ord(c)
+  if c.isalpha():
+    pos = (ord(c)+2-ord("a"))%26 + ord("a")
+
+  answer += chr(pos)
+
+print(answer)
