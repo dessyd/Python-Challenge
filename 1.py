df178638853df5e808dcc7e120d2d59870e4b319
dessyd <reprendre.mezzo_0j@icloud.com>
2023-01-21 18:03:12 +0100

    Create decode function

--- 1.py
+++ 1.py
@@ -1,11 +1,17 @@
 riddle = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
-answer = ""
 
-for c in riddle:
-    pos = ord(c)
-    if c.isalpha():
-        pos = (ord(c)+2-ord("a")) % 26 + ord("a")
 
-    answer += chr(pos)
+def decode(riddle):
+    answer = ""
+    for c in riddle:
+        pos = ord(c)
+        if c.isalpha():
+            pos = (ord(c)+2-ord("a")) % 26 + ord("a")
 
-print(answer)
+        answer += chr(pos)
+
+    return answer
+
+
+print(decode(riddle))
+print(decode("map"))
